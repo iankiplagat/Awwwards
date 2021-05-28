aec4d52419e95477a72a22a763c9bf8a1def3e0c
Kasparov <ianjkiplagat@gmail.com>
2021-05-28 06:28:09 +0300

    create templates

--- Awwwards/settings.py
+++ Awwwards/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'proHub.apps.ProhubConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
