35bc79484cbee67a82d1ad217fb838331fcd82fa
Kasparov <ianjkiplagat@gmail.com>
2021-05-28 13:43:00 +0300

    initiate profile tests

--- proHub/tests.py
+++ proHub/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Profile, User
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User()
+        self.user.save()
+        self.profile = Profile(user = self.user, profile_pic = 'img', name = 'img', bio = 'I am Levlest')
+        
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
