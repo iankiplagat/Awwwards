4b0e7d1ddadd820cc2ddc06fa0e468292a3aff10
Kasparov <ianjkiplagat@gmail.com>
2021-05-28 14:58:14 +0300

    add registration and login

--- proHub/urls.py
+++ proHub/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.homepage, name = 'home'),
+    path('create_profile/', views.create_profile, name='create_profile'),
 ]
--- proHub/views.py
+++ proHub/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import CreateProfileForm
+from django.http import HttpResponseRedirect,Http404
 
 # Create your views here.
 @login_required
 def homepage(request):
   
   return render(request, 'home.html')
+
+@login_required
+def create_profile(request):
+  current_user=request.user
+  if request.method == 'POST':
+    form = CreateProfileForm(request.POST,request.FILES)
+    if form.is_valid():
+      profile = form.save(commit=False)
+      profile.user = current_user
+      profile.save()
+    return HttpResponseRedirect('/')
+  else:
+    form = CreateProfileForm()
+  return render(request,'profile/create_profile.html',{"form":form})
