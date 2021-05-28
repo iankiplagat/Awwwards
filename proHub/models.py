c875f027dbb0e2e316fd8bc7196a5966224949d0
Kasparov <ianjkiplagat@gmail.com>
2021-05-28 14:13:26 +0300

    create login page

--- proHub/models.py
+++ proHub/models.py
@@ -6,9 +6,19 @@ from django.db.models.fields import TextField
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = CloudinaryField('photo', blank=True)
+    profile_pic = CloudinaryField('profile_pic', blank=True)
     name =  models.CharField(max_length =30, blank=True)
     bio = models.CharField(max_length =200, blank=True)
   
     def __str__(self):
         return self.user.username
+      
+    def save_profile(self):
+        self.save()
+          
+    def delete_profile(self):
+        self.delete()
+        
+    @classmethod
+    def update_profile(cls,id,profile_pic,name,bio):
+        cls.objects.filter(id=id).update(profile_pic=profile_pic,name=name,bio=bio)   
--- proHub/tests.py
+++ proHub/tests.py
@@ -12,3 +12,23 @@ class ProfileTestClass(TestCase):
     # Testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.profile,Profile))
+        
+    # Testing Save Method
+    def test_save_method(self):  
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+        
+    def tearDown(self):
+        Profile.objects.all().delete()
+     
+    # Testing Delete Method  
+    def delete_profile(self):
+        self.delete() 
+        
+    # Testing Update Method    
+    def test_update_profile(self):
+        self.profile.save_profile()
+        self.profile.update_profile(self.profile.id,'img', 'name', 'I am Levlest')
+        update=Profile.objects.get(profile_pic='img',name='name',bio='I am Levlest')
+        self.assertEqual(update.bio,'I am Levlest')                          
