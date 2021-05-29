a9bb89b4a487a22476bb053a69a393e1025d9a32
Kasparov <ianjkiplagat@gmail.com>
2021-05-29 08:51:43 +0300

    style login page

--- proHub/urls.py
+++ proHub/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('', views.homepage, name = 'home'),
@@ -7,3 +9,5 @@ urlpatterns=[
     path('create_profile/', views.create_profile, name='create_profile'),
     path('profile/<username>',views.user_profile,name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
